3a92e9705f9f1e46869c616918c19d3942499657
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-06-23 17:13:46 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -26,7 +26,7 @@ like= message_data['like'];
 
 name_width_tag= '<h4>' + name + '</h4>' ;
 message_width_tag= '<h4 class="message_h4">' + message + '</h4>';
-like_button= "<button class='btn btn-warning' id="+ firebase_message_id + "value=" + like + "onclick='updateLike(this.id)'>";
+like_button= "<button class='btn btn-warning' id="+ firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
 span_width_tag= "<span class='glyphicon glyphicon-thumbs-up'>  Like: " + like + "</span> </button> <hr>";
 row= name_width_tag + message_width_tag + like_button + span_width_tag;
 document.getElementById("output").innerHTML+= row;
